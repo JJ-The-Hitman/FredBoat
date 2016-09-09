bf334a8f9cd896009b320d7d69360ac0fef3af3b
Frederik Ar. Mikkelsen <frogkr@gmail.com>
2016-09-09 17:08:00 +0200

    Added #isTextChannelMusicLocked()

--- src/main/java/fredboat/db/RedisGuildCache.java
+++ src/main/java/fredboat/db/RedisGuildCache.java
@@ -2,6 +2,10 @@ package fredboat.db;
 
 import fredboat.util.BotConstants;
 import java.util.Map;
+import java.util.function.Consumer;
+import net.dv8tion.jda.entities.TextChannel;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class RedisGuildCache {
 
@@ -20,6 +24,15 @@ public class RedisGuildCache {
         return data;
     }
     
+    public JSONObject getSettings() {
+        String raw = getData().get("settings");
+        if(raw != null){
+            return new JSONObject(raw);
+        } else {
+            return new JSONObject();
+        }
+    }
+    
     public String getPrefix(){
         Map<String, String> d = getData();
         return d.getOrDefault("prefix", BotConstants.DEFAULT_BOT_PREFIX);
@@ -33,6 +46,31 @@ public class RedisGuildCache {
 
     private void query() {
         data = RedisCache.jedis.hgetAll("guild:" + guild);
+    }
+    
+    /* READ - Helper functions */
+    
+    public boolean isTextChannelMusicLocked(TextChannel tc){
+        JSONObject settings = getSettings();
+        
+        if(settings.has("musicLockAllowedChannels")){
+            JSONArray allowed = settings.getJSONArray("musicLockAllowedChannels");
+            
+            if(allowed.length() == 0){
+                return false;
+            }
+            
+            for(Object str : allowed){
+                if(str.equals(tc.getId())){
+                    return false;
+                }
+            }
+            
+            //Only if there are values and they don't match our given channel
+            return true;
+        }
+        
+        return false;
     }
 
 }
